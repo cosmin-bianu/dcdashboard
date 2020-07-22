d0f8ffbb8d046613665fd527aeb3e0245fa0e27c
Cosmin Bianu <kitsune.cosmin@gmail.com>
2020-07-22 09:05:19 +0300

    Added Questionnaire models

--- data_management/models.py
+++ data_management/models.py
@@ -4,3 +4,12 @@ from django.db import models
 #TODO
 # Create your models here.
 class Questionnaire:
+    max_question_length=100 #characters
+    max_answer_characters=100 #characters
+    max_answers=4
+
+    question_id=models.SmallAutoField(primary_key=True)
+    question=models.models.CharField(max_length=max_question_length)
+    #answers=
+    answer_count=models.PositiveSmallIntegerField()
+    correct_answer_index=models.PositiveSmallIntegerField()
