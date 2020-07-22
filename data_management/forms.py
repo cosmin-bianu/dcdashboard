3e70921574b5a29aad5d2d23290731062c96bba6
Cosmin Bianu <kitsune.cosmin@gmail.com>
2020-07-22 17:32:49 +0300

    Added Chapter Creation form

--- data_management/forms.py
+++ data_management/forms.py
@@ -1,5 +1,15 @@
 from django import forms
+from data_management.models import Chapter,TwoAnswerExercise,FourAnswerExercise,Course
 
 class LoginForm(forms.Form):
     username=forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'input is-rounded is-primary is-medium', 'placeholder':'Nume de utilizator'}))
     password=forms.CharField(max_length=60, widget=forms.PasswordInput(attrs={'class': 'input is-rounded is-primary is-medium', 'placeholder':'Parolă'}))
+
+
+class ChapterCreationForm(forms.ModelForm):
+    class Meta:
+        model = Chapter
+        fields = ["name",]
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'input is-medium', 'placeholder':'Numele capitolului'}),
+        }
